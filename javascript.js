e572e60e56cc9a3b7378b388ae17013a0639c965
ImDadir <abdulqadirsheikh04@gmail.com>
2025-02-23 20:51:40 -0500

    Fixed the style.css to say styles.css in the title.

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ const todoInput = document.getElementById('todo-input');
 const addButton = document.getElementById('add-button');
 const todoList = document.getElementById('todo-list');
 
+
 addButton.addEventListener('click', function () {
     const task = todoInput.value;
 
